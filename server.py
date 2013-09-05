f2f31f666cf12556288dffe075667309ba665ed0
Petr Demin <PDemin@iteranet.ru>
2013-09-05 09:27:46 +0400

    ROC server: Added package discovery

--- server.py
+++ server.py
@@ -1,47 +1,77 @@
 import threading
+import collections
 from SimpleXMLRPCServer import SimpleXMLRPCServer
-from remote_fixture import PowFixture
-PowFixture  # lint: ok
 
 
 def main():
-    server = createServer()
+    server = create_server()
     print "Listening on port 8000..."
     server.serve_forever()
 
 
-def createServer(logRequests=True):
+def create_server(logRequests=True):
     server = SimpleXMLRPCServer(("127.0.0.1", 8000),
                                 allow_none=True,
                                 logRequests=logRequests)
     server.register_introspection_functions()
     server.register_function(
-        lambda: threading.Thread(target=server.shutdown).start(),
+        lambda: stop_server(server),
         "shutdown"
+    )
+    available_classes = import_classes()
+    existing_instances = collections.defaultdict(lambda: [])
+    server.register_function(
+        lambda: available_classes.keys(),
+        "classes"
+    )
+    server.register_function(
+        lambda: existing_instances.keys(),
+        "instances"
     )
 
     @server.register_function
     def create(class_name, args=None):
-        instance_name = class_name + '_inst'
-        Class = globals()[class_name]
-        instance = Class(*args)
-        for item_name in Class.__dict__:
-            if '__' not in item_name:
-                item = getattr(instance, item_name)
-                if callable(item):
-                    name = '.'.join([instance_name, item_name])
-                    # print 'register_function', name
-                    server.register_function(item, name)
+        instance = available_classes[class_name]['class'](*args)
+        instance_idx = len(existing_instances[class_name])
+        instance_name = '%s_%d' % (class_name, instance_idx)
+        for method_name in available_classes[class_name]['methods']:
+            bound_name = '.'.join([instance_name, method_name])
+            bound_method = getattr(instance, method_name)
+            server.register_function(bound_method, bound_name)
+        existing_instances[class_name].append(instance)
         return instance_name
     return server
 
 
-def startServer(logRequests=True):
+def start_server(logRequests=True):
     thread = threading.Thread(
-        target=createServer(logRequests).serve_forever
+        target=create_server(logRequests).serve_forever
     )
     thread.start()
     return thread
+
+
+def stop_server(server):
+    thread = threading.Thread(target=server.shutdown)
+    thread.start()
+    thread.join()
+
+
+def import_classes(fixtures_package_name='fixtures'):
+    import inspect
+    fixtures = __import__(fixtures_package_name)
+    modules = inspect.getmembers(fixtures, inspect.ismodule)
+    classes = {}
+    for _, module in modules:
+        for class_name, Class in inspect.getmembers(module, inspect.isclass):
+            methods = [n
+                       for n, _ in inspect.getmembers(Class, inspect.ismethod)
+                       if '__' not in n]
+            classes[class_name] = {
+                'class': Class,
+                'methods': methods,
+            }
+    return classes
 
 
 if __name__ == '__main__':
