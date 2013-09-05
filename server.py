b93d42190e1425719be7b83823d3719ccf64d905
Petr Demin <PDemin@iteranet.ru>
2013-09-05 11:41:17 +0400

    Made import classes recursive and refactored server

--- server.py
+++ server.py
@@ -1,77 +1,91 @@
-import threading
+import itertools
 import collections
+import threading
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 
 
 def main():
+    # print import_classes()
     server = create_server()
     print "Listening on port 8000..."
     server.serve_forever()
 
 
-def create_server(logRequests=True):
-    server = SimpleXMLRPCServer(("127.0.0.1", 8000),
-                                allow_none=True,
-                                logRequests=logRequests)
-    server.register_introspection_functions()
-    server.register_function(
-        lambda: stop_server(server),
-        "shutdown"
-    )
-    available_classes = import_classes()
-    existing_instances = collections.defaultdict(lambda: [])
-    server.register_function(
-        lambda: available_classes.keys(),
-        "classes"
-    )
-    server.register_function(
-        lambda: existing_instances.keys(),
-        "instances"
-    )
-
-    @server.register_function
-    def create(class_name, args=None):
-        instance = available_classes[class_name]['class'](*args)
-        instance_idx = len(existing_instances[class_name])
-        instance_name = '%s_%d' % (class_name, instance_idx)
-        for method_name in available_classes[class_name]['methods']:
-            bound_name = '.'.join([instance_name, method_name])
-            bound_method = getattr(instance, method_name)
-            server.register_function(bound_method, bound_name)
-        existing_instances[class_name].append(instance)
-        return instance_name
-    return server
-
-
 def start_server(logRequests=True):
+    def serve_forever_or_die_trying():
+        server = create_server(logRequests)
+        server.serve_forever()
+        server.shutdown()  # break request_handle loop
+        server.server_close()  # close socket
     thread = threading.Thread(
-        target=create_server(logRequests).serve_forever
+        target=serve_forever_or_die_trying
     )
     thread.start()
     return thread
 
 
-def stop_server(server):
-    thread = threading.Thread(target=server.shutdown)
-    thread.start()
-    thread.join()
+def create_server(logRequests=True):
+    return RocServer(logRequests).server
 
 
-def import_classes(fixtures_package_name='fixtures'):
-    import inspect
-    fixtures = __import__(fixtures_package_name)
-    modules = inspect.getmembers(fixtures, inspect.ismodule)
+class RocServer(object):
+    def __init__(self, logRequests=True):
+        self.server = SimpleXMLRPCServer(("127.0.0.1", 8000),
+                                         allow_none=True,
+                                         logRequests=logRequests)
+        self.available_classes = import_classes()
+        self.existing_instances = collections.defaultdict(lambda: [])
+        self.server.register_introspection_functions()
+        self.server.register_instance(self)
+
+    def shutdown(self):
+        threading.Thread(target=self.server.shutdown).start()
+
+    def classes(self):
+        return self.available_classes.keys()
+
+    def instances(self):
+        return list(itertools.chain(*self.existing_instances.values()))
+
+    def create(self, class_name, args=None):
+        if args is None:
+            args = []
+        instance = self.available_classes[class_name]['class'](*args)
+        instance_idx = len(self.existing_instances[class_name])
+        instance_name = '%s_%d' % (class_name, instance_idx)
+        for method_name in self.available_classes[class_name]['methods']:
+            bound_name = '.'.join([instance_name, method_name])
+            bound_method = getattr(instance, method_name)
+            self.server.register_function(bound_method, bound_name)
+        self.existing_instances[class_name].append(instance_name)
+        return instance_name
+
+
+def import_classes(package_path='fixtures'):
+    from inspect import getmembers, isclass, ismethod
     classes = {}
-    for _, module in modules:
-        for class_name, Class in inspect.getmembers(module, inspect.isclass):
+    for module in import_modules(package_path):
+        for class_name, Class in getmembers(module, isclass):
             methods = [n
-                       for n, _ in inspect.getmembers(Class, inspect.ismethod)
+                       for n, _ in getmembers(Class, ismethod)
                        if '__' not in n]
             classes[class_name] = {
                 'class': Class,
                 'methods': methods,
             }
     return classes
+
+
+def import_modules(package_path='fixtures'):
+    import os
+    import pkgutil
+    for loader, name, is_pkg in pkgutil.walk_packages([package_path]):
+        if is_pkg:
+            subpackage_path = os.path.join(package_path, name)
+            for module in import_modules(subpackage_path):
+                yield module
+        else:
+            yield loader.find_module(name).load_module(name)
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import xmlrpclib
 import unittest
 from pow_fixture import PowFixture
 from remote_class import RemoteClass
-from server import startServer
+from server import start_server, import_classes, import_modules
 
 
 class PowFixtureTestCase(unittest.TestCase):
@@ -25,19 +25,48 @@ class RemoteClassTestCase(unittest.TestCase):
 
 class RealServerTestCase(unittest.TestCase):
     def setUp(self):
-        self.server_thread = startServer(logRequests=False)
+        self.server_thread = start_server(logRequests=False)
         self.proxy = xmlrpclib.ServerProxy(
             "http://127.0.0.1:8000/",
             allow_none=True
         )
+        self.remote = lambda name: RemoteClass(self.proxy, name)
+
+    def test_whats_registered(self):
+        must_be_provided = ('classes', 'create', 'instances', 'shutdown')
+        provided_methods = self.proxy.system.listMethods()
+        self.assertTrue(all([method in provided_methods
+                             for method in must_be_provided]))
 
     def test_remote_power(self):
-        PowFixture = RemoteClass(self.proxy, 'PowFixture')
+        PowFixture = self.remote('PowFixture')
         self.assertEqual(PowFixture(3).pow(2), 9)
+
+    def test_server_instances(self):
+        self.assertEqual(self.proxy.instances(), [])
+        self.proxy.create('PowFixture', [2])
+        self.proxy.create('PowFixture', [5])
+        self.assertEqual(sorted(self.proxy.instances()),
+                         ['PowFixture_0', 'PowFixture_1'])
+        self.assertEqual(self.proxy.PowFixture_0.pow(10),
+                         1024)
+        self.assertEqual(self.proxy.PowFixture_1.pow(2),
+                         25)
 
     def tearDown(self):
         self.proxy.shutdown()
         self.server_thread.join()
+
+
+class InspectionTestCase(unittest.TestCase):
+    def test_import_modules_founds_submodules(self):
+        modules = [m.__name__ for m in import_modules()]
+        self.assertEqual(modules, ['pow_fixture', 'div_fixture'])
+
+    def test_import_classes_founds_submodules(self):
+        classes = import_classes()
+        self.assertEqual(sorted(classes.keys()),
+                         ['DivFixture', 'PowFixture'])
 
 
 if __name__ == '__main__':
