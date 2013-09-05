edc15182aa7eb2ad1208a215e7fcb9689ec814ad
Petr Demin <PDemin@iteranet.ru>
2013-09-05 14:16:33 +0400

    CLI: added --folder

--- src/cli.py
+++ src/cli.py
@@ -1,15 +1,16 @@
-from optparse import OptionParser
-from .server import create_server
-
-
 def main():
+    from optparse import OptionParser
+    from .server import create_server
+
     parser = OptionParser()
-    parser.add_option("-p", "--port", help="Server port")
-    parser.add_option("-d", "--dir", help="Package directory path")
+    parser.add_option("-p", "--port", type="int", help="Server port")
+    parser.add_option("-d", "--folder", default='.',
+                      help="Package directory path")
     parser.add_option("-q", "--quiet",
                       action="store_false", dest="verbose", default=True,
                       help="don't print log messages to stdout")
     (options, args) = parser.parse_args()
     if options.verbose:
         print "Listening on port %d..." % (options.port)
-    create_server(options.port, options.verbose).serve_forever()
+    server = create_server(options.folder, options.port, options.verbose)
+    server.serve_forever()
