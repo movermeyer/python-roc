717677cb946b1263fa3b1f1f2788062e378190f6
Petr Demin <PDemin@iteranet.ru>
2013-09-11 12:36:31 +0400

    Make it more like a package

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,11 @@
 from .server import start_server, create_server
 from .remote_class import RemoteClass
+from .client import create_remote
+from .cli import main
 
 
-__all__ = (start_server, create_server, RemoteClass)
+__all__ = (start_server, create_server, RemoteClass, create_remote)
 
 
 if __name__ == '__main__':
-    from .cli import main
     main()
--- src/client.py
+++ src/client.py
@@ -1,4 +1,5 @@
 import six
+from .remote_class import RemoteClass
 
 
 def create_remote(host='127.0.0.1', port=8000):
@@ -6,12 +7,8 @@ def create_remote(host='127.0.0.1', port=8000):
         from xmlrpclib import ServerProxy
     elif six.PY3:
         from xmlrpc.client import ServerProxy
-    from .remote_class import RemoteClass
     connection = ServerProxy(
         "http://%s:%d/" % (host, port),
         allow_none=True
     )
     return connection, lambda name: RemoteClass(connection, name)
-
-if __package__ is None:
-    __package__ = "roc.client"
