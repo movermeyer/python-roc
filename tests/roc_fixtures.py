e2b14e608b656d68ac7472f33f3d131ecdc94814
Petr Demin <PDemin@iteranet.ru>
2013-09-11 13:15:29 +0400

    Added waferslim smoke test

--- tests/roc_fixtures.py
+++ tests/roc_fixtures.py
@@ -1,7 +1,8 @@
 import os
 import sys
+sys.path.insert(0, os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 import test_data
-sys.path.insert(0, os.path.join('..'))
 from src import client
 from src import server
 
